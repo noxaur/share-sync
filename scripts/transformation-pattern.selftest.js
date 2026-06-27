b8f6643ac17fad5b5de7da2249b578f2bad1637d
Per <noxaur@users.noreply.github.com>
2026-06-27 12:25:43 +0200

    Guard IndexHtml patch to HTML documents

--- scripts/transformation-pattern.selftest.js
+++ scripts/transformation-pattern.selftest.js
@@ -2,6 +2,7 @@ const assert = require('node:assert/strict');
 const fs = require('node:fs');
 
 const source = fs.readFileSync('Jellyfin.Plugin.SyncPlayShare/Services/StartupService.cs', 'utf8');
+const patchSource = fs.readFileSync('Jellyfin.Plugin.SyncPlayShare/Helpers/TransformationPatches.cs', 'utf8');
 const match = source.match(/RegisterTransformation\([\s\S]*?"([^"]+)",\s*nameof\(TransformationPatches\.IndexHtml\)\)/);
 
 assert.ok(match, 'IndexHtml transformation registration not found');
@@ -10,5 +11,7 @@ const pattern = new RegExp(match[1].replace(/\\\\/g, '\\'));
 
 assert.equal(pattern.test('index.html'), true);
 assert.equal(pattern.test('/session/login/index.html'), false);
+assert.equal(pattern.test('session-login-index-html.7df1620bd3afcef60eb7.chunk.js'), false);
+assert.match(patchSource, /IsHtmlDocument\(contents\)/);
 
 console.log('transformation pattern self-check passed');
